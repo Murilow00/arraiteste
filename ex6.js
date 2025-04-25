8c7958ccadc78f51d291f1c9687393b726377265
Murilow <milanmurilo7@gmail.com>
2025-04-25 10:30:38 -0300

    ex6 da lista

---
+++ ex6.js
@@ -0,0 +1,5 @@
+let OrdemParanormal =['Sinais', 'Calamidade', 'Desconjuração', 'Natal Macabro', 'Segredo Na Floresta']
+let quantidade = OrdemParanormal.length
+for(let i = quantidade-1;i > -1; i--){
+    console.log(OrdemParanormal[i])
+}
